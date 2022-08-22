2c1b5b87464f5e4df074bda9c2d69b1b4943466b
jheart-vic <adebowalevictorjheart@gmail.com>
2022-08-22 20:00:41 +0100

    Fix the changes required

--- person.rb
+++ person.rb
@@ -2,10 +2,11 @@ class Person
   attr_reader :id
   attr_accessor :age, :name
 
-  def initialize(age, name)
+  def initialize(age, name = "Unknown", parent_permission: true)
     @id = DateTime.now
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
 
   def of_age?
@@ -13,7 +14,7 @@ class Person
   end
 
   def can_use_services?
-    of_age?
+    of_age? || @parent_permission
   end
 
   private :of_age
