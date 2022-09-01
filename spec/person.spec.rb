2b80d57571b818585f55a9ec3a123ef231b0a4a9
jheart-vic <adebowalevictorjheart@gmail.com>
2022-09-01 10:58:20 +0100

    Add more test for methods

--- spec/person.spec.rb
+++ spec/person.spec.rb
@@ -1,13 +1,36 @@
 require './classes/person'
+require './classes/rentals'
+require './classes/book'
 
 describe Person do
   context 'test Person class' do
     person = Person.new(24, 'Mechael Obama')
+    person2 = Person.new(24)
+    book = Book.new(nil, 'Becoming', 'Mechael Obama')
+    rental = Rental.new('2022-05-13', book, person)
+    it 'Book title should return Becoming' do
+      expect(rental.book.title).to eq 'Becoming'
+    end
     it 'Person age should return 24' do
       expect(person.age).to eq 24
     end
     it 'Person name should return Mechael Obama' do
       expect(person.name).to eq 'Mechael Obama'
+    end
+    it 'Person correct_name should return Mechael Obama' do
+      expect(person.correct_name).to eq 'Mechael Obama'
+    end
+    it 'Person can_use_service should return Mechael Obama' do
+      expect(person.can_use_services?).to be true
+    end
+    it 'Person of_age should return true' do
+      expect(person.send(:of_age?)).to be true
+    end
+    it 'Person name should return Mechael Obama' do
+      expect(person2.name).to eq 'unknown'
+    end
+    it 'Person parent_permission should return true' do
+      expect(person.parent_permission).to be true
     end
   end
 end
--- spec/student.spec.rb
+++ spec/student.spec.rb
@@ -9,5 +9,14 @@ describe Student do
     it 'Student name should return Mechael Obama' do
       expect(student.name).to eq 'Mechael Obama'
     end
+    it 'Person parent_permission should return true' do
+      expect(student.parent_permission).to be true
+    end
+    it 'Person classroom should return nil' do
+      expect(student.classroom).to be nil
+    end
+    it 'Person play_hooky should return nil' do
+      expect(student.play_hooky).to eq "¯\(ツ)/¯"
+    end
   end
 end
